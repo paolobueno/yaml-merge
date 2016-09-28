f22176820b9ff65540fb84cc9d0129650b7115a9
Paolo Bueno <paolohaji@gmail.com>
2016-09-28 18:10:00 -0300

    Use only merge from lodash

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
-const _ = require('lodash');
+const merge = require('lodash.merge');
 
 const fromPath = path.resolve(process.argv[2]);
 const patchPath = path.resolve(process.argv[3]);
@@ -10,6 +10,6 @@ const patchPath = path.resolve(process.argv[3]);
 const original = yaml.safeLoad(fs.readFileSync(fromPath));
 const patch = yaml.safeLoad(fs.readFileSync(patchPath));
 
-var newObj = _.merge(original, patch);
+var newObj = merge(original, patch);
 
 console.log(yaml.safeDump(newObj));
